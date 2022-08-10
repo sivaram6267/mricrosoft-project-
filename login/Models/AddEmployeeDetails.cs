c320439f1e62678f5e30653c8aea83ff77e200e9
sivaram6267 <90128892+sivaram6267@users.noreply.github.com>
2022-08-10 07:43:47 -0400

    Update AddEmployeeDetails.cs

--- login/Models/AddEmployeeDetails.cs
+++ login/Models/AddEmployeeDetails.cs
@@ -23,7 +23,7 @@ namespace login.Models
         public DateTime? Po_start_Date { get; set; }
         public DateTime? Po_end_date { get; set; }
         public int Tenure { get; set; }
-        public decimal PaidTillNow { set; get; }
+        public decimal? PaidTillNow { set; get; }
         public decimal Profit_OR_loss { get; set; }
         public int Bench_Tenure { get; set; }
         public decimal Bench_expences { get; set; }
